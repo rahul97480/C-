e1715047140a00cf2696c10967b95b74731c52ec
Rahul Gupta <guptarahul97480@gmail.com>
2021-04-02 15:22:58 +0530

    Insertion Rectified

--- insertion.c
+++ insertion.c
@@ -18,7 +18,7 @@ void insertion(int arr[], int n){
     for(int i=1;i<n;i++){
       int temp = arr[i];
       int j = i-1;
-      while(j>= n && arr[j] > temp){
+      while(j>= 0 && arr[j] > temp){
           arr[j+1] = arr[j];
           j--;
       }
